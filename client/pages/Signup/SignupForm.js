1b2e2882b435812f4b0df4c525720b0ae2b5702d
chirwaden <chirwad@hotmail.com>
2017-01-03 00:34:35 +0000

    removed bootstrap classes

--- client/pages/Signup/SignupForm.js
+++ client/pages/Signup/SignupForm.js
@@ -4,20 +4,53 @@ class SignupForm extends React.Component {
 constructor(props) {
     super(props);
     this.state = {
-        username:''
+        name:'',
+        username:'',
+        email:'',
+        password:'',
+        passwordConfirmation:''
     }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 }
+
+onChange(e) {
+    this.setState({ [e.target.name]: e.target.value});
+}
+
+onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+}
+
 
     render() {
         return (
-            <form className="row">
+            <form onSubmit={this.onSubmit}>
                 <h1>Sign up with us!</h1>
-                <div className="form-group">
-                   <label className="control-label">Username</label>
-                   <input value={this.state.username} type="text" name="username" className="form-control" />
+                <div>
+                   <label>Name</label>
+                   <input value={this.state.name} onChange={this.onChange} type="text" name="name"/>
                 </div>
-                <div className="form-group">
-                   <button className="btn btn-primary btn-lg">Sign up</button>
+                <div>
+                   <label>Username</label>
+                   <input value={this.state.username} onChange={this.onChange} type="text" name="username"/>
+                </div>
+                <div>
+                   <label>Email</label>
+                   <input value={this.state.email} onChange={this.onChange} type="text" name="email"/>
+                </div>
+                <div>
+                   <label>Password</label>
+                   <input value={this.state.password} onChange={this.onChange} type="password" name="password"/>
+                </div>
+                <div>
+                   <label>Password Confirmation</label>
+                   <input value={this.state.passwordConfirmation} onChange={this.onChange} type="password" name="passwordConfirmation"/>
+                </div>
+                <div>
+                   <button>Sign up</button>
 
                 </div>
             </form>
