417d9c9edc061534028683ef096e1506f9487910
chirwaden <chirwad@hotmail.com>
2017-01-03 02:21:56 +0000

    Migrated navigation CSS into navigation component

--- client/components/NavigationBar/NavigationBar.js
+++ client/components/NavigationBar/NavigationBar.js
@@ -1,11 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+    display: flex;
+    justify-content: center;
+    width: 100vw;
+    background: #007EE5;
+`;
+
+const NavWrapper = styled.section`
+    flex: 1;
+    display: flex;
+    justify-content: flex-start;
+    height: 160px;
+    width: 100vw;
+    max-width: 1400px;
+    
+    > .navigationBar_bar {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: flex-start;
+        width: 100%;
+
+        > .navigationBar {
+            margin: 20px;
+        
+            > a {
+                color: #FFFFFF !important;
+                text-decoration: none;
+                text-transform: uppercase;
+                font-size: 1em;
+                font-weight: 300;
+            }
+            > a:hover {
+                text-decoration: underline;
+            }
+        } 
+    }
+
+    > .navigationBar_username {
+        justify-content: flex-end !important;
+    }
+`;
+
+
+
 
 export default () => {
     return (
-        <div className="navigationContainer">
-        
-            <div className="navigationBarContainer">
+        <Wrapper>       
+            <NavWrapper>
                 <div className="navigationBar_bar">
                     <div className="navigationBar navigationBar_dasboard">
                         <Link to="/" href="#">Dashboard</Link>
@@ -26,8 +72,8 @@ export default () => {
                         <a href="#">Jane Appleseed</a>
                     </div>
                 </div>
-            </div>
-        </div>
+            </NavWrapper>
+        </Wrapper>
 
     );
 }
--- client/pages/App.js
+++ client/pages/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import NavigationBar from '../components/NavigationBar/NavigationBar';
 import Dashboard from '../pages/Dashboard/DashboardPage';
+import styled from 'styled-components';
+
+
+const Wrapper = styled.div`
+    > @import {url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,800')};
+    justify-content: flex-start;
+    align-content: flex-start;
+    flex-direction: row;
+    height: 100vh;
+    font-family: 'Open Sans', sans-serif
+    color: #47525D
+`;
 
 class App extends React.Component {
     render() {
         return (
-            <div className="container">
+            <Wrapper>
                 <NavigationBar />
                 {this.props.children}
-            </div>
+            </Wrapper>
             
         );
     }
