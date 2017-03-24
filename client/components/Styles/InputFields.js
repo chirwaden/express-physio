5d26c85e597545533d0a7c16faadd1a7227776a4
chirwaden <chirwad@hotmail.com>
2017-03-24 10:23:16 +0000

    fixed border radius issues for input fields

--- client/components/Styles/InputFields.js
+++ client/components/Styles/InputFields.js
@@ -12,12 +12,13 @@ const Input = styled.input`
     border-radius: 3px;
     width: 100%;
     margin: 7px 0px 30px 0px;
-    transition: outline .2s, border .2s ease-in-out;
+    outline:none;
 
     &:focus{
-        outline: 2px solid #4BBC73;
-        border: 1px solid #4BBC73;
+        outline:none;
+        border: 2px solid #4BBC73;
         border-radius: 3px;
+        padding: 14px;
     }
 
 `;
