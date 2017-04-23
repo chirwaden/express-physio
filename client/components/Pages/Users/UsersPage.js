e2415d13276a19923498bf99ed6942ff6a0f80b2
Dennis Chirwa <dchirwa@ukm0317328b2.connect>
2017-04-23 14:47:44 +0100

    Table styles, navigation adjustments

--- client/components/Pages/Users/UsersPage.js
+++ client/components/Pages/Users/UsersPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import Main from '../PagesWrapper';
 import NavigationBar from '../../NavigationBar/NavigationBar';
+import Table from '../../Styles/Tables';
+import Checkbox from '../../Styles/Checkbox';
 
 class UsersPage extends React.Component {
     render() {
@@ -16,6 +18,41 @@ class UsersPage extends React.Component {
                 </div>
                 <div className="PageWrapper">
                 <p>Some Content Here</p>
+
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox"></input>
+                                    <span className="label-text"></span>
+                                </label>
+                            </th>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Organisation</th>
+                            <th>State</th>
+                            <th>Last Active</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><input type="checkbox"/></td>
+                            <td><span className="avatarInitials">YA</span></td>
+                            <td>Yvonne Archer</td>
+                            <td>YVonA@htomal.com</td>
+                            <td>Standard</td>
+                            <td>The Smart Physio</td>
+                            <td>Active</td>
+                            <td>9 months ago</td>
+                            <td>Menu</td>
+                        </tr>
+                    </tbody>
+                </Table>
+        
                 </div>
             </Main>
         
