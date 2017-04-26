33347153ec1e5781f01bd8f6ac83c21bbeb928df
Dennis Chirwa <dchirwa@ukm0317328b2.corp.iparadigms.com>
2017-04-26 16:33:20 +0100

    webpack change

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -25,27 +25,11 @@ export default {
         loaders: [
 
             {
-                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'file-loader'
-            },
-            {
-                test: /\.(png|jpg)$/, 
-                loader: 'url-loader?limit=8192'
-            },
-            {
                 test: /\.jsx$/,
                 include: path.join(__dirname, 'client'),
                 loaders: ['react-hot','babel?presets[]=stage-0']
             },
             {
-<<<<<<< HEAD
-=======
-                test: /\.jsx$/,
-                include: path.join(__dirname, 'client'),
-                loaders: ['react-hot','babel?presets[]=stage-0']
-            },
-            {
->>>>>>> 2b78ba0da48c1880b9d1b3a3fb0019732dbd810e
                 test: /\.js$/,
                 include: path.join(__dirname, 'client'),
                 loaders: ['react-hot','babel?presets[]=stage-0']
