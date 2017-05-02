1dc4872fa82ddf569719f359c5672c7b66aa11a2
chirwaden <chirwad@hotmail.com>
2017-05-02 14:55:25 +0100

    removed broken modal code

--- client/components/Pages/Users/UsersProfilePage.js
+++ client/components/Pages/Users/UsersProfilePage.js
@@ -91,15 +91,6 @@ class UserProfilePage extends React.Component {
                             </Detail>
                         </Attribute>
 
-                        <Modal isOpen={this.state.modalIsOpen} onCancel={this.toggleModal} backdropClosesModal>
-                            <ModalHeader text="Lots of text to show scroll behavior" showCloseButton onClose={this.toggleModal} />
-                            <ModalBody>[...]</ModalBody>
-                            <ModalFooter>
-                                <Button type="primary" onClick={this.toggleModal}>Close modal</Button>
-                                <Button type="link-cancel" onClick={this.toggleModal}>Also closes modal</Button>
-                            </ModalFooter>
-                        </Modal>
-
                         <Attribute>
                             <Label>Name</Label>
                             <Detail>
